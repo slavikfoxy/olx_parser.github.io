12a9f0fb01e66ac903794e392e9c982957420288
slavikfoxy <oliynik.v2014@gmail.com>
2025-05-18 14:08:42 +0200

    new version multi

--- OLX-parser.py
+++ OLX-parser.py
@@ -5,16 +5,19 @@ import time
 from datetime import datetime
 import os
 from telegram import Bot
-from telegram import Update, InputMediaPhoto
+from telegram import InputMediaPhoto
 from telegram.helpers import escape_markdown
 import asyncio
-import json
 import logging
+import re
 
 # Конфігурація
-#SEARCH_URL = "https://www.olx.pl/elektronika/sprzet-audio/glosniki-i-kolumny/q-magnat/?courier=1"
+SEARCH_URLS = [
+    "https://www.olx.pl/elektronika/sprzet-audio/glosniki-i-kolumny/q-magnat/?courier=1",
+    "https://www.olx.pl/elektronika/sprzet-audio/glosniki-i-kolumny/q-jbl-kolumny/?courier=1"
+    # Додайте інші URL-адреси за потреби
+]
 HEADERS = {"User-Agent": "Mozilla/5.0"}
-JSON_FILE = "olx_ads.json"
 TOKEN = '7930055889:AAEG1rcIRftxKxzIRzqAxTj8TaWpd2c-fNQ'
 CHAT_ID = '376481898'
 bot = Bot(token=TOKEN)
@@ -27,32 +30,40 @@ log_file = os.path.join("logs", f"{datetime.now().strftime('%Y-%m-%d')}.log")
 
 # Налаштування логування
 logging.basicConfig(
-    level=logging.INFO,  # Рівень логів (DEBUG, INFO, WARNING, ERROR, CRITICAL)
-
-    format="%(asctime)s - %(levelname)s - %(message)s",  # Формат повідомлення
-    datefmt="%Y-%m-%d %H:%M:%S",  # Формат дати й часу
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
     handlers=[
-        logging.StreamHandler(),  # Виведення в консоль
-        logging.FileHandler(log_file, encoding="utf-8")  # Запис у файл
+        logging.StreamHandler(),
+        logging.FileHandler(log_file, encoding="utf-8")
     ]
 )
 
-"""
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привіт! Я OLX Бот 🛎️")
-"""
-async def load_old_ads():
-    if os.path.exists(JSON_FILE):
-        with open(JSON_FILE, "r", encoding="utf-8") as f:
+def get_json_filename(url):
+    """Генерує ім'я JSON-файлу на основі параметра запиту в URL."""
+    query = re.search(r'q-([^/?]+)', url)
+    if query:
+        return f"{query.group(1)}.json"
+    return "default.json"  # Запасне ім'я, якщо параметр не знайдено
+
+async def load_old_ads(json_file):
+    """Завантажує старі оголошення з JSON-файлу."""
+    if os.path.exists(json_file):
+        with open(json_file, "r", encoding="utf-8") as f:
             return json.load(f)
     return []
 
-async def save_ads(data):
-    with open(JSON_FILE, "w", encoding="utf-8") as f:
+async def save_ads(data, json_file):
+    """Зберігає оголошення у JSON-файл."""
+    with open(json_file, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=2, ensure_ascii=False)
 
-async def generate_html_from_json(json_path: str, html_output: str = "index.html"):
-    with open(json_path, "r", encoding="utf-8") as f:
+async def generate_html_from_json(json_file, html_output=None):
+    """Генерує HTML-файл із JSON-даними."""
+    if not html_output:
+        html_output = f"{os.path.splitext(json_file)[0]}.html"
+    
+    with open(json_file, "r", encoding="utf-8") as f:
         ads = json.load(f)
 
     html_parts = ['<html><head><meta charset="utf-8"><title>OLX Ads</title>']
@@ -102,19 +113,19 @@ async def generate_html_from_json(json_path: str, html_output: str = "index.html
     with open(html_output, "w", encoding="utf-8") as f:
         f.write("".join(html_parts))
 
-    #print(f"✅ HTML файл збережено: {html_output}")
     logging.info(f"✅ HTML файл збережено: {html_output}")
 
-async def parse_ads(old_links):
+async def parse_ads(url, old_links):
+    """Парсить оголошення для заданого URL."""
     ads = []
     seen_links = set()
     last_page = set()
     this_page = set()
     page = 1
-    itteration = 0
+    iteration = 0
     while True:
-        url = f"https://www.olx.pl/elektronika/sprzet-audio/glosniki-i-kolumny/q-magnat/?page={page}"
-        response = requests.get(url, headers=HEADERS)
+        paginated_url = f"{url}&page={page}" if '?' in url else f"{url}?page={page}"
+        response = requests.get(paginated_url, headers=HEADERS)
         soup = BeautifulSoup(response.text, "html.parser")
         cards = soup.select("div[data-cy='l-card']")
         
@@ -126,21 +137,18 @@ async def parse_ads(old_links):
             link_tag = offer.select_one("a.css-1tqlkj0")
             title_tag = offer.select_one("h4")
             price_tag = offer.select_one("p[data-testid='ad-price']")
-            #await asyncio.sleep(0.1)  # 1 секунда між запитами
-            itteration += 1
+            iteration += 1
+
             if link_tag and title_tag:
-                #print(f"{itteration}: {title_tag.get_text(strip=True)} ")
-               
                 relative_link = link_tag.get("href")
                 link = "https://www.olx.pl" + relative_link
                 
                 if link in seen_links:
-                    continue  # це вже було
+                    continue
                 this_page.add(link)
                 seen_links.add(link)
                 
-                if not link in old_links:
-                    
+                if link not in old_links:
                     ad = {
                         "title": title_tag.get_text(strip=True),
                         "link": link,
@@ -149,69 +157,55 @@ async def parse_ads(old_links):
                         "date_found": datetime.now().strftime("%Y-%m-%d %H:%M"),
                         "date_removed": None
                     }
-                    await parse_ad_details(ad)  # <- доповнюємо інформацію
+                    await parse_ad_details(ad)
                     if not any(f in ad["description"] for f in filter):
                         logging.info(f"NEW {ad}")
                         new_ads.append(ad)
-                else:
-                    #print(f"Пропускаємо {link} (вже в базі)")
-                    continue
-                
-        """
-        if not cards:
-            print(f"No new ads on page {page}. Stopping.")
-            break
-        """
+
         last_page = this_page.copy()
         this_page.clear()
         
         if last_page == this_page:
-            #print(f"Last page {page} is the same as this page. Stopping.")
             logging.info(f"Last page {page} is the same as this page. Stopping.")
             break
         
         ads.extend(new_ads)
-        #print(f"Parsed page {page}, found {len(new_ads)} new ads.")
         if len(new_ads) > 0:
             logging.info(f"Parsed page {page}, found {len(new_ads)} new ads.")
         page += 1
         await asyncio.sleep(1)
-        
 
     return ads
 
 async def parse_ad_details(ad):
+    """Парсить деталі оголошення."""
     response = requests.get(ad["link"], headers=HEADERS)
     soup = BeautifulSoup(response.text, "html.parser")
 
-    # Опис
     desc_tag = soup.select_one("div[class='css-19duwlz']")
     ad["description"] = desc_tag.get_text(strip=True) if desc_tag else ""
 
-     # Всі зображення з класу swiper-zoom-container
     image_urls = []
     for div in soup.select("div.swiper-zoom-container img"):
         src = div.get("src")
         if src and src.startswith("http"):
             image_urls.append(src)
-
     ad["images"] = image_urls
     
-    # Дата публікації (іноді прихована)
-    #posted_info = soup.find("span", string=re.compile(r"dnia"))
     posted_info = soup.select_one("span[class='css-1eaxltp']")
     if posted_info:
         posted_info = posted_info.get_text(strip=True)
         posted_info = posted_info.replace("Dodane", "").strip()
-        if not posted_info.find("Dzisiaj"):
+        if "Dzisiaj" in posted_info:
             posted_info = posted_info.replace("Dzisiaj", "").strip()
-            posted_info = f"{datetime.now().strftime("%Y-%m-%d")} {posted_info}"
+            posted_info = f"{datetime.now().strftime('%Y-%m-%d')} {posted_info}"
         ad["published_date"] = posted_info
         
     location_tag = soup.select_one("p[data-testid='location-date']")
     ad["location"] = location_tag.get_text(strip=True) if location_tag else ""
 
 async def notify_new_ads(new_ads):
+    """Надсилає повідомлення про нові оголошення."""
     await asyncio.sleep(1)
     for ad in new_ads:
         message = (
@@ -220,72 +214,62 @@ async def notify_new_ads(new_ads):
             f"📍 {ad['location']}\n"
             f"🔗 [Перейти на OLX]({ad['link']})"
         )
-        #message2 = (f"{ad['description'][:1021] + '...'}")
         message2 = escape_markdown(ad['description'][:1021] + '...', version=2)
         images = ad.get("images") or []
         if images:
-            media_group = [InputMediaPhoto(media=img) for img in images[:10]]  # max 10
+            media_group = [InputMediaPhoto(media=img) for img in images[:10]]
             try:
                 await bot.send_media_group(chat_id=CHAT_ID, media=media_group)
             except Exception as e:
-                #print(f"❌ Помилка надсилання фото: {e}")
                 logging.error(f"❌ Помилка надсилання фото: {e}")
         
-        # Надіслати окремо повідомлення з caption, оскільки caption в media_group показується лише до першої картинки
         await bot.send_message(chat_id=CHAT_ID, text=message, parse_mode="Markdown", disable_web_page_preview=True)
-        await bot.send_message(chat_id=CHAT_ID, text=message2, parse_mode="MarkdownV2", disable_web_page_preview=True) #MarkdownV2 - escape_markdown
-        
-        
+        await bot.send_message(chat_id=CHAT_ID, text=message2, parse_mode="MarkdownV2", disable_web_page_preview=True)
+
 async def main():
-    old_ads = await load_old_ads()
-    old_links = {ad["link"]: ad for ad in old_ads}
-    old_titles = {ad["title"]: ad for ad in old_ads}
-    #print(f"Found old {len(old_links)} ads.")
-    logging.info(f"Found old {len(old_links)} ads.")
-    current_ads = await parse_ads(old_links)
-    #print(f"Found actual {len(current_ads)} ads.")
-    logging.info(f"Found actual {len(current_ads)} ads.")
-    current_links = {ad["link"] for ad in current_ads}
-    today = datetime.now().strftime("%Y-%m-%d")
+    """Основна функція для обробки всіх URL-адрес."""
+    for url in SEARCH_URLS:
+        json_file = get_json_filename(url)
+        logging.info(f"Обробка URL: {url}, JSON: {json_file}")
+        
+        old_ads = await load_old_ads(json_file)
+        old_links = {ad["link"]: ad for ad in old_ads}
+        old_titles = {ad["title"]: ad for ad in old_ads}
+        logging.info(f"Знайдено старих оголошень: {len(old_links)}")
+        
+        current_ads = await parse_ads(url, old_links)
+        logging.info(f"Знайдено актуальних оголошень: {len(current_ads)}")
+        current_links = {ad["link"] for ad in current_ads}
+        today = datetime.now().strftime("%Y-%m-%d")
 
-    updated_ads = []
+        updated_ads = []
 
-    # Додаємо або оновлюємо активні оголошення
-    for ad in current_ads:
-        if (ad["link"] not in old_links) or (ad["title"] not in old_titles):
-            logging.info(f"Додаємо {ad["link"]}.")
-            ad["date_found"] = today
-            ad["status"] = "active"
-            ad["date_removed"] = None
-            await notify_new_ads([ad])
-            updated_ads.append(ad)
-        else:
-            existing = old_links[ad["link"]]
-            ad["date_found"] = existing.get("date_found") or today
-            ad["status"] = "active"
-            ad["date_removed"] = None
-            updated_ads.append(ad)
+        # Додаємо або оновлюємо активні оголошення
+        for ad in current_ads:
+            if (ad["link"] not in old_links) or (ad["title"] not in old_titles):
+                logging.info(f"Додаємо {ad['link']}.")
+                ad["date_found"] = today
+                ad["status"] = "active"
+                ad["date_removed"] = None
+                #await notify_new_ads([ad])
+                updated_ads.append(ad)
+            else:
+                existing = old_links[ad["link"]]
+                ad["date_found"] = existing.get("date_found") or today
+                ad["status"] = "active"
+                ad["date_removed"] = None
+                updated_ads.append(ad)
 
-    # Позначаємо як неактивні ті, що зникли
-    for link, ad in old_links.items():
-        if link not in current_links:
-            if ad.get("status") != "inactive":
-                ad["status"] = "inactive"
-                ad["date_removed"] = today
-            updated_ads.append(ad)
+        # Позначаємо як неактивні ті, що зникли
+        for link, ad in old_links.items():
+            if link not in current_links:
+                if ad.get("status") != "inactive":
+                    ad["status"] = "inactive"
+                    ad["date_removed"] = today
+                updated_ads.append(ad)
               
-    await save_ads(updated_ads)
-    
-    await generate_html_from_json("olx_ads.json")
-    
-"""
-def main0():
-    app = ApplicationBuilder().token(TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    loop = asyncio.get_event_loop()
-    loop.create_task(main())
-    app.run_polling()
-"""
+        await save_ads(updated_ads, json_file)
+        await generate_html_from_json(json_file)
 
 if __name__ == "__main__":
     asyncio.run(main())
